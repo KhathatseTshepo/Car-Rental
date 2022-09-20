e11254c6d3a497a8d225396f1a068f618256f11b
khathatse Tshepo <113677540+KhathatseTshepo@users.noreply.github.com>
2022-09-20 11:40:24 +0200

    Add files via upload

---
+++ Car Rental/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Car_Rental
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(40, 58);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1 Day";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(40, 125);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(114, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2 DAYS";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(40, 198);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(114, 29);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3 DAYS";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(478, 58);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(114, 29);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4 DAYS";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.Location = new System.Drawing.Point(478, 125);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(114, 29);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5 DAYS";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton6.Location = new System.Drawing.Point(478, 198);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(114, 29);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6 DAYS";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(35, 330);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(84, 25);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "TOTAL";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(447, 317);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(145, 38);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // cmb1
+            // 
+            this.cmb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Items.AddRange(new object[] {
+            "COMPACT",
+            "STANDARD",
+            "LUXURY"});
+            this.cmb1.Location = new System.Drawing.Point(210, 259);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(382, 33);
+            this.cmb1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Car Style";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(677, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Car Rentals";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Car Rental/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Rental
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            
+                const double compact = 328.27;
+                const double standard = 410.54;
+                const double luxury = 642.82;
+                double sum;
+                
+
+                if (radioButton1.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                }
+                else if (radioButton2.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact * 2;
+                        
+                    lblTotal.Text ="R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard * 2;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury * 2;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                }
+                else if (radioButton3.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact * 3;
+                    lblTotal.Text = "R" + Convert.ToString(sum); 
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard * 3;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury * 3;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                }
+                else if (radioButton4.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact * 4;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard * 4;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury * 4;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                }
+                else if (radioButton5.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact * 5;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard * 5;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury * 5;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+
+                }
+                else if (radioButton6.Checked)
+                {
+                    if (cmb1.Text == "COMPACT")
+                    {
+                        sum = compact * 6;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "STANDARD")
+                    {
+                        sum = standard * 6;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                    else if (cmb1.Text == "LUXURY")
+                    {
+                        sum = luxury * 6;
+                    lblTotal.Text = "R" + Convert.ToString(sum);
+                }
+                }
+
+        }
+    }
+}
